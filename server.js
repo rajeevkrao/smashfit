0b302a1a5450c03d6285ac6ddf172b6c653551de
Rakshit <rakshujp007@gmail.com>
2021-12-22 10:35:31 +0530

    project details changes

--- server.js
+++ server.js
@@ -13,8 +13,5 @@ app.use(express.static('public'))
 app.get('/',(req,res)=>{
 	res.sendFile(__dirname+"/index.html")
 })
-app.get('/main',(req,res)=>{
-	res.sendFile(__dirname+"/main.html")
-})
 
 app.listen(5000)
