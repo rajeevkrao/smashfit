9b6bad915671329ecbaee2d9aa47f641bb3b28be
rakshujp <destello004@gmail.com>
2021-12-28 10:34:26 +0530

    small changes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyparser = require("body-parser")
+const bodyParser = require("body-parser")
 require('dotenv').config();
 
 var auth = require("./modules/auth.js")
