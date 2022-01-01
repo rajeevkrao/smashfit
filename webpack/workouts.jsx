eed8b523d9b3f6942de798e0800cd665956e4ec7
RahulRao06 <rahulprao6@gmail.com>
2022-01-01 19:28:56 +0530

    bundles #2

--- webpack/workouts.jsx
+++ webpack/workouts.jsx
@@ -6,7 +6,10 @@ import { Route, Routes } from'react-router-dom';
 
 import Hit from'./workouts/hit.js';
 import BalTrain from'./workouts/baltrain.js';
-import MuscleTraining from "./workouts/muscletraining.js"
+import MuscleTraining from "./workouts/muscletraining.js";
+import Cardio from "./workouts/cardio.js";
+import FullBody from "./workouts/fullbody.js";
+import StrengthTraining from "./workouts/sttraining.js"
 
 render((
 <BrowserRouter>
@@ -14,6 +17,9 @@ render((
     <Route path="/workouts/hit" element={<Hit />} />
 	  <Route path="/workouts/baltrain" element={<BalTrain />} />
     <Route path="/workouts/muscletraining" element={<MuscleTraining />} />
+    <Route path="/workouts/cardio" element={<Cardio/>} />
+    <Route path="/workouts/fullbody" element={<FullBody />} />
+    <Route path="/workouts/sttraining" element={<StrengthTraining />} />
   </Routes>
 </BrowserRouter> 
 ), document.getElementById('App'));
