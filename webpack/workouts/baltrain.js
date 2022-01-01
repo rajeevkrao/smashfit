666975561ad6cbdcf86787bbd22fd4653cff799b
rakshujp <destello004@gmail.com>
2022-01-01 18:50:15 +0530

    path changes

--- webpack/workouts/baltrain.js
+++ webpack/workouts/baltrain.js
@@ -28,24 +28,24 @@ class App extends Component {
                         </div>
                         <div className="row">
                             <div className="col-xl-6 col-lg-6 col-md-6">
-                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\flutter-kick.gif"/>
+                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\without equipments\flutter-kick.gif"/>
                             </div>
                             <div className="col-xl-6 col-lg-6 col-md-6">
-                            	<img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\bicycle.gif"/> 
+                            	<img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\without equipments\bicycle.gif"/> 
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-xl-6 col-lg-6 col-md-6">
-                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\Inchworm.gif"/> 
+                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\without equipments\Inchworm.gif"/> 
                             </div>
                             <div className="col-xl-6 col-lg-6 col-md-6">
-                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\plank-Taps.gif"/> 
+                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\without equipments\plank-Taps.gif"/> 
                             </div>
                             <div className="col-xl-6 col-lg-6 col-md-6">
-                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\push-up.gif"/> 
+                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\without equipments\push-up.gif"/> 
                             </div>
                             <div className="col-xl-6 col-lg-6 col-md-6">
-                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\Triceps-Dips.gif"/> 
+                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\without equipments\Triceps-Dips.gif"/> 
                             </div>
                         </div>
                     </div>
