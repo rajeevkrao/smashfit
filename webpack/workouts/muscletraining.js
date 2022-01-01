ae7736f73270bde26411f72bb2b5fbcd53add5db
RahulRao06 <rahulprao6@gmail.com>
2022-01-01 19:04:26 +0530

    gif path changes

--- webpack/workouts/muscletraining.js
+++ webpack/workouts/muscletraining.js
@@ -22,30 +22,30 @@ class App extends Component {
                         <div className="row">
                             <div className="col-xl-9 col-lg-9 col-md-10">
                                 <div className="hero__caption">
-                                    <span data-animation="fadeInLeft" data-delay="0.09s">High Intensity Training</span>
+                                    <span data-animation="fadeInLeft" data-delay="0.09s">Muscle Training</span>
                                 </div>
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-xl-6 col-lg-6 col-md-6">
-                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\flutter-kick.gif"/>
+                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\without equipments\flutter-kick.gif"/>
                             </div>
                             <div className="col-xl-6 col-lg-6 col-md-6">
-                            	<img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\bicycle.gif"/> 
+                            	<img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\without equipments\bicycle.gif"/> 
                             </div>
                         </div>
                         <div className="row">
                             <div className="col-xl-6 col-lg-6 col-md-6">
-                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\Inchworm.gif"/> 
+                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\without equipments\Inchworm.gif"/> 
                             </div>
                             <div className="col-xl-6 col-lg-6 col-md-6">
-                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\plank-Taps.gif"/> 
+                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\without equipments\plank-Taps.gif"/> 
                             </div>
                             <div className="col-xl-6 col-lg-6 col-md-6">
-                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\push-up.gif"/> 
+                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\without equipments\push-up.gif"/> 
                             </div>
                             <div className="col-xl-6 col-lg-6 col-md-6">
-                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\Triceps-Dips.gif"/> 
+                                <img className="gifs" data-animation="fadeInLeft" data-delay="0.09s" src="..\assets\img\gifs\without equipments\Triceps-Dips.gif"/> 
                             </div>
                         </div>
                     </div>
